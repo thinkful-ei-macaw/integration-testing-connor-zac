6baf4d88d980ff7f01ec299f3100f94451ab315c
Connor McCormack <mccormack.connor.m@gmail.com>
2020-03-25 14:26:55 -0500

    halfway thru drills

--- test/divide.test.js
+++ test/divide.test.js
@@ -1,29 +1,46 @@
-const divide = require('../index');
+const sort = require('../index');
 const { expect } = require('chai');
 
-it('should divide positive integers correctly', () => {
-  // define inputs
-  const a = 8;
-  const b = 4;
-  const expectedAnswer = 2;
+// it('should divide positive integers correctly', () => {
+//   // define inputs
+//   const a = 8;
+//   const b = 4;
+//   const expectedAnswer = 2;
 
-  // invoke the function
-  const actualAnswer = divide(a, b);
+//   // invoke the function
+//   const actualAnswer = divide(a, b);
 
-  // assert that expected === actual
-  expect(actualAnswer).to.equal(expectedAnswer);
-});
+//   // assert that expected === actual
+//   expect(actualAnswer).to.equal(expectedAnswer);
+// });
 
-it('should throw an error when divide by zero', () => {
-  // define inputs
-  const a = 8, b = 0;
+// it('should throw an error when divide by zero', () => {
+//   // define inputs
+//   const a = 8, b = 0;
 
-  // set up the function call
-  const fn = () => {
-    divide(a, b)
-  };
+//   // set up the function call
+//   const fn = () => {
+//     divide(a, b)
+//   };
 
-  // assert that exception is thrown
-  expect(fn).to.throw();
-});
+//   // assert that exception is thrown
+//   expect(fn).to.throw();
+// });
 
+describe('List sorting function', () => {
+  it('should sort a list correctly', () => {
+    // define inputs 
+    const arr1 = ['a','c','b'];
+    const arr2 = ['a','b','c'];
+    const expectedAnswer = arr2;
+    // invoke function
+    const actualAnswer = sort(arr1);
+    // assert that expected === actual
+    expect(actualAnswer).to.deep.equal(expectedAnswer);
+  });
+  it('should test equality correctly', () => {
+    const a = { x: 5 };
+    const b = { x: 5 };
+    expect(a).to.deep.equal(b);    
+  })
+})
