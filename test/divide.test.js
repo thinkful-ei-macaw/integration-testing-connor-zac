17a7edb7f63eaacf1de637a21446453f459aa55f
Connor McCormack <mccormack.connor.m@gmail.com>
2020-03-25 13:14:09 -0500

    drills

---
+++ test/divide.test.js
@@ -0,0 +1,29 @@
+const divide = require('../index');
+const { expect } = require('chai');
+
+it('should divide positive integers correctly', () => {
+  // define inputs
+  const a = 8;
+  const b = 4;
+  const expectedAnswer = 2;
+
+  // invoke the function
+  const actualAnswer = divide(a, b);
+
+  // assert that expected === actual
+  expect(actualAnswer).to.equal(expectedAnswer);
+});
+
+it('should throw an error when divide by zero', () => {
+  // define inputs
+  const a = 8, b = 0;
+
+  // set up the function call
+  const fn = () => {
+    divide(a, b)
+  };
+
+  // assert that exception is thrown
+  expect(fn).to.throw();
+});
+
